87ffdd9ab456e967b4487a5f108c1cb7cb459c68
Felix Clark <mfclark3690@gmail.com>
2021-07-02 13:05:25 -0400

    calling pip robustly

--- setup_python.sh
+++ setup_python.sh
@@ -3,6 +3,6 @@
 # Install neovim python support. This should maybe be in setup.sh; other plugings may use it.
 # This will need to be installed in virtual environments as well, I think.
 # We need a version of neovim with python3 support.
-pip3 install pynvim --upgrade
+python3 -m pip install pynvim --upgrade
 
 nvim -c 'CocInstall\ coc-pyright' -c '<\CR>' -c 'qa'
