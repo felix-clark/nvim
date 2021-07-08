3fd0eb72f64d46a897f723767b59cee91d27216c
Felix Clark <mfclark3690@gmail.com>
2021-07-08 19:18:51 -0400

    nvim-tree line extension

--- lualine.lua
+++ lualine.lua
@@ -15,5 +15,5 @@ require'lualine'.setup{
     lualine_y = {'encoding', 'fileformat', 'filetype'},
     lualine_z = {'progress', 'location'},
   },
-  extensions = { 'fugitive' },
+  extensions = { 'fugitive', 'nvim-tree' },
 }
