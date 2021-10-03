b08d3da440c8b4f313754561eaab5c8774136dd1
Felix Clark <mfclark3690@gmail.com>
2021-10-03 08:51:27 -0400

    sexier folds

--- lua/fold.lua
+++ lua/fold.lua
@@ -1,9 +1,38 @@
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
--- vim.o.fillchars = "fold:\\"
--- don't fill the fold lines with a character
-vim.o.fillchars = "fold: "
+
+function _G.my_fold_text ()
+  -- NOTE: If tabs are in the foldline, they may be rendered as single-width.
+  -- This is the vim:
+  -- vim.o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').' ...  (' . (v:foldend - v:foldstart + 1) . ' lines [' . (v:foldlevel) . '])']]
+  -- Something like this would replace them, although it's choking on repeat()
+  -- local first_line = vim.fn.substitute(
+  --   vim.fn.getline(vim.v.foldstart),
+  --   '\\t',
+  --   vim.fn.repeat('\\ ', vim.v.tabstop),
+  --   'g')
+  local first_line = vim.fn.getline(vim.v.foldstart)
+  local line_count = vim.v.foldend - vim.v.foldstart + 1
+  local filler = ' ... '
+  if (string.sub(vim.fn.trim(first_line), -1) == '{') then
+    local last_char = string.sub(vim.fn.trim(vim.fn.getline(vim.v.foldend)), -1)
+    if (last_char == '}') then
+      filler = filler .. last_char .. ' '
+    end
+  end
+  local count_str = ' (' .. line_count .. ' lines)'
+  -- NOTE: Could indicate the fold level with something like this:
+  -- local level_str = ' [' .. vim.v.foldlevel .. ']'
+  -- local level_str = ' ' .. vim.v.folddashes
+  -- TODO: Try to include the closing bracket after the dots if the final char
+  -- of the last line is one
+  return first_line .. filler .. count_str
+end
+
+vim.o.foldtext = 'v:lua.my_fold_text()'
+vim.o.foldcolumn = 'auto:3'
+-- don't fill the fold lines with a character, use space
+vim.o.fillchars = 'fold: '
 -- consider reducing to 3
 vim.o.foldnestmax = 4
 -- 1 seems to be the default anyway
