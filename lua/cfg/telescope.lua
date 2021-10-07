a5103435df6909d5a86e4b86a673dd8cb14f2fa2
Felix Clark <mfclark3690@gmail.com>
2021-10-07 19:13:18 -0400

    style

--- lua/cfg/telescope.lua
+++ lua/cfg/telescope.lua
@@ -1,27 +1,27 @@
-local actions = require('telescope.actions')
-local trouble = require('trouble.providers.telescope')
+local actions = require("telescope.actions")
+local trouble = require("trouble.providers.telescope")
 
-require'telescope'.setup{
-  defaults = {
-    mappings = {
-      i = {
-        -- exit telescope with a single press of escape
-        ["<esc>"] = actions.close,
-        -- use C-h for help. Default is <C-/> or ? for i or n mode.
-        ["<C-h>"] = "which_key",
-        ["<C-t>"] = trouble.open_with_trouble,
-      },
-      n = {
-        ["<C-t>"] = trouble.open_with_trouble,
-      },
-    },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = false,
-      override_file_sorter = true,
-      case_mode = "smart_case", -- or "ignore_case", "respect_case"
-    },
-  },
-}
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				-- exit telescope with a single press of escape
+				["<esc>"] = actions.close,
+				-- use C-h for help. Default is <C-/> or ? for i or n mode.
+				["<C-h>"] = "which_key",
+				["<C-t>"] = trouble.open_with_trouble,
+			},
+			n = {
+				["<C-t>"] = trouble.open_with_trouble,
+			},
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = false,
+			override_file_sorter = true,
+			case_mode = "smart_case", -- or "ignore_case", "respect_case"
+		},
+	},
+})
