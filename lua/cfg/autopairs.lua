246f802494e82a406dbde785653d2df562083531
Felix <felix@geneticintelligence.com>
2021-11-01 20:58:11 +0000

    update cmp/autopairs

--- lua/cfg/autopairs.lua
+++ lua/cfg/autopairs.lua
@@ -29,17 +29,9 @@ npairs.add_rules({
 npairs.get_rule("'")[2]:with_pair(ts_conds.is_not_ts_node({"type_arguments", "bounded_type"}))
 
 -- cmp integration
--- nvim-autopairs documentation says to remove cmp <CR> mapping
-require('nvim-autopairs.completion.cmp').setup({
-  map_cr = true, -- map <CR> on insert mode
-  map_complete = true, -- will auto-insert '(' ('map_char') after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = {
-    all = '(',
-    tex = '{',
-  },
-})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } } ))
 
 local M = {}
 M.toggle_autopairs = function()
