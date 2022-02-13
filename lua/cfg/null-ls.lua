545983efea1dc4b26cc0bed914b5610815ff6966
Felix Clark <mfclark3690@gmail.com>
2022-02-13 12:26:08 -0500

    formatting

--- lua/cfg/null-ls.lua
+++ lua/cfg/null-ls.lua
@@ -1,20 +1,20 @@
-local null_ls = require("null-ls")
-null_ls.setup({
-	sources = {
+local null_ls = require "null-ls"
+null_ls.setup {
+  sources = {
     -- lua
     null_ls.builtins.formatting.stylua,
 
-		-- Python
+    -- Python
     -- null_ls.builtins.formatting.autopep8,
     null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.isort,
-		null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.diagnostics.flake8,
     -- pylint uses a lot of memory/CPU and is largely redundant with pylsp
-		-- null_ls.builtins.diagnostics.pylint,
-		null_ls.builtins.diagnostics.mypy,
+    -- null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.mypy,
 
-		-- web
-		null_ls.builtins.formatting.prettier,
-	},
+    -- web
+    null_ls.builtins.formatting.prettier,
+  },
   on_attach = require("cfg.lsp").on_attach,
-})
+}
