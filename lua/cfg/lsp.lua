1057105374c90271c22620979d3d922eca62ad8e
Felix Clark <mfclark3690@gmail.com>
2022-03-09 13:05:56 -0500

    try to turn off telemetry

--- lua/cfg/lsp.lua
+++ lua/cfg/lsp.lua
@@ -192,6 +192,10 @@ local lua_settings = {
         [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
       },
     },
+    -- Do not send telemetry data containing a randomized but unique identifier
+    telemetry = {
+      enable = false,
+    },
   },
 }
 
