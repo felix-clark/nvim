caf498b6b8245a7c1f031f3f0032105e4d262ca6
felix-clark <mfclark3690@gmail.com>
2022-10-13 16:17:40 +0000

    removing python tools from null-ls

--- lua/cfg/null-ls.lua
+++ lua/cfg/null-ls.lua
@@ -8,13 +8,16 @@ null_ls.setup {
     null_ls.builtins.formatting.stylua,
 
     -- Python
+    -- NOTE: The main ones here can all be included in the pylsp installation
+    -- with nvim-lsp-installer:
+    -- https://github.com/williamboman/nvim-lsp-installer/blob/main/lua/nvim-lsp-installer/servers/pylsp/README.md
     -- null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.diagnostics.flake8,
+    -- null_ls.builtins.formatting.black,
+    -- null_ls.builtins.formatting.isort,
+    -- null_ls.builtins.diagnostics.flake8,
     -- pylint uses a lot of memory/CPU and is largely redundant with pylsp
     -- null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.diagnostics.mypy,
+    -- null_ls.builtins.diagnostics.mypy,
 
     -- web
     null_ls.builtins.formatting.prettier,
