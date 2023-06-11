afa9ab63d614c9c4ee9a41c7c27a065dc3b0bf19
Felix Clark <mfclark3690@gmail.com>
2023-06-11 17:06:06 -0400

    point to mason's debugpy python executable

--- lua/cfg/dap/python.lua
+++ lua/cfg/dap/python.lua
@@ -1,7 +1,16 @@
 local M = {}
 
 function M.setup(_)
-  require("dap-python").setup("python", {})
+  -- Need to point to a python executable that has debugpy installed. Mason
+  -- should provide one.
+  local debugpy_bin_dir = vim.fn.stdpath "data" .. "/mason/packages/debugpy/venv/bin/"
+  local python_path = debugpy_bin_dir .. "python"
+  if vim.fn.executable(python_path) ~= 1 then
+    print "Could not find python executable in mason's debugpy install."
+    python_path = "python"
+  end
+
+  require("dap-python").setup(python_path)
 end
 
 return M
