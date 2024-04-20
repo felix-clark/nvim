7d2d34d13cefad3405f9b1e819dd831cbaa9e87e
Felix Clark <mfclark3690@gmail.com>
2024-04-20 16:59:11 -0400

    decoupling whichkey from nvim-dap config

--- lua/cfg/dap/keymaps.lua
+++ lua/cfg/dap/keymaps.lua
@@ -1,55 +1,42 @@
+local dap = require "dap"
+local dapui = require "dapui"
+-- see :h dap-widgets for more to do with these, although much is covered by
+-- dap-ui.
+local widgets = require "dap.ui.widgets"
+
+local function nmap(lhs, rhs, desc)
+  local opts = { silent = true, desc = desc }
+  vim.keymap.set("n", lhs, rhs, opts)
+end
+
 local M = {}
-
-local whichkey = require "which-key"
-
 function M.setup()
-  local keymap = {
-    d = {
-      name = "Debug",
-      R = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run to Cursor" },
-      E = { "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input" },
-      C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
-      U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
-      b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
-      c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-      d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
-      e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
-      g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
-      h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
-      S = { "<cmd>lua require'dap.ui.widgets'.scopes()<cr>", "Scopes" },
-      i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-      o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-      p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
-      q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
-      r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
-      s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
-      t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-      x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
-      u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
-    },
-  }
+  nmap("<leader>dR", dap.run_to_cursor, "Run to Cursor")
+  nmap("<leader>dE", function()
+    dapui.eval(vim.fn.input "[Expression] > ")
+  end, "Evaluate Input")
+  nmap("<leader>dC", function()
+    dap.set_breakpoint(vim.fn.input "[Condition] > ")
+  end, "Conditional Breakpoint")
+  nmap("<leader>dU", dapui.toggle, "Toggle UI")
+  nmap("<leader>db", dap.step_back, "Step Back")
+  nmap("<leader>dc", dap.continue, "Continue")
+  nmap("<leader>dd", dap.disconnect, "Disconnect")
+  nmap("<leader>de", dapui.eval, "Evaluate")
+  nmap("<leader>dg", dap.session, "Get Session")
+  nmap("<leader>dh", widgets.hover, "Hover Variables")
+  nmap("<leader>di", dap.step_into, "Step Into")
+  nmap("<leader>do", dap.step_over, "Step Over")
+  nmap("<leader>dp", dap.pause.toggle, "Pause")
+  nmap("<leader>dq", dap.close, "Quit")
+  nmap("<leader>dr", dap.repl.toggle, "Toggle Repl")
+  nmap("<leader>ds", dap.continue, "Start")
+  nmap("<leader>dt", dap.toggle_breakpoint, "Toggle Breakpoint")
+  nmap("<leader>dx", dap.terminate, "Terminate")
+  nmap("<leader>du", dap.step_out, "Step Out")
 
-  whichkey.register(keymap, {
-    mode = "n",
-    prefix = "<leader>",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = false,
-  })
-
-  local keymap_v = {
-    name = "Debug",
-    e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
-  }
-  whichkey.register(keymap_v, {
-    mode = "v",
-    prefix = "<leader>",
-    buffer = nil,
-    silent = true,
-    noremap = true,
-    nowait = false,
-  })
+  -- evalutes the highlighted expression
+  vim.keymap.set("v", "<leader>e", dapui.eval, { silent = true, desc = "Evaluate" })
 end
 
 return M
