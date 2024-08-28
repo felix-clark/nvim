335b0fc1890859ccd79bca55507f42e099c9efa2
Felix Clark <mfclark3690@gmail.com>
2024-08-28 17:11:36 -0400

    some which-key branches in visual mode too

--- lua/cfg/which-key.lua
+++ lua/cfg/which-key.lua
@@ -8,18 +8,22 @@ wk.add {
       return require("which-key.extras").expand.buf()
     end,
   },
-  { "<leader>c", group = "code" },
   { "<leader>d", group = "debug" },
   { "<leader>f", group = "file" },
-  { "<leader>g", group = "git" },
   { "<leader>l", group = "lsp" },
-  -- { "<leader>m", group = "local" }, -- in old config this caused some clunky interactions; hasn't been tested in updated yet
   { "<leader>o", group = "open" },
-  { "<leader>r", group = "refactor" },
   { "<leader>s", group = "search" },
   { "<leader>t", group = "toggle" },
   { "<leader>w", proxy = "<C-w>", group = "windows" },
   { "g", group = "goto" },
   { "]", group = "next" },
   { "[", group = "previous" },
+  {
+    -- branches with options in both normal and visual mode
+    mode = { "n", "v" },
+    { "<leader>c", group = "code" },
+    { "<leader>g", group = "git" },
+    -- { "<leader>m", group = "local" }, -- in old config this caused some clunky interactions; hasn't been tested in updated yet
+    { "<leader>r", group = "refactor" },
+  },
 }
