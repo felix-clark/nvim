0577106be36349d822b75bd873c1befcaa8998e0
Felix Clark <felix@genetic-leap.com>
2024-09-05 12:52:05 +0000

    robustifying setup script and including lazygit

--- setup.sh
+++ setup.sh
@@ -12,19 +12,18 @@ then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     source $HOME/.cargo/env
 fi
-cargo install ripgrep fd-find bat git-delta
-# lua formatting. More recently maintained than luafmt, which is written in
-# TypeScript.
-cargo install stylua
+rustup update
+cargo install ripgrep fd-find bat git-delta --locked
 
 # Commented out because it has not been tested.
-# if ! command -v lazygit &> /dev/null
-# then
-#   # Install lazygit using recommended procedure for ubuntu.
-#   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-#   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-#   tar xf lazygit.tar.gz lazygit
-#   sudo install lazygit /usr/local/bin
-# fi
+if ! command -v lazygit &> /dev/null
+then
+  # Install lazygit using recommended procedure for ubuntu.
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+  rm -r lazygit lazygit.tar.gz
+fi
 
 echo "Make sure to install a NERDfont and set it as your terminal's font."
