1ba424f40f6a903519900a575ea67fe70855e3a1
Felix Clark <mfclark3690@gmail.com>
2025-11-06 16:58:42 -0500

    formatting and add output toggle

--- lua/cfg/test.lua
+++ lua/cfg/test.lua
@@ -3,33 +3,45 @@ local M = {}
 M.keys = {
   {
     "<leader>Tr",
-    function() 
-    require("neotest").run.run() end,
+    function()
+      require("neotest").run.run()
+    end,
     desc = "Run nearest test",
   },
   {
     "<leader>Tf",
     function()
-    require("neotest").run.run(vim.fn.expand "%") end,
+      require("neotest").run.run(vim.fn.expand "%")
+    end,
     desc = "Run tests in current file",
   },
   {
     "<leader>Td",
     function()
-    require("neotest").run.run { strategy = "dap" } end,
+      require("neotest").run.run { strategy = "dap" }
+    end,
     desc = "Debug nearest test",
   },
   {
     "<leader>Tq",
     function()
-    require("neotest").run.stop() end,
+      require("neotest").run.stop()
+    end,
     desc = "Stop nearest test",
   },
   {
     "<leader>Ts",
     function()
-    require("neotest").summary.toggle() end,
+      require("neotest").summary.toggle()
+    end,
     desc = "Toggle summary window",
+  },
+  {
+    "<leader>Tt",
+    function()
+      require("neotest").output_panel.toggle()
+    end,
+    desc = "Toggle output window",
   },
 }
 
