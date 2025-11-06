8a430f56667ece9aeb8eab5a8894aaf0a5e81eb3
Felix Clark <mfclark3690@gmail.com>
2025-11-06 10:12:36 -0500

    wrap in function for proper lazy loading

--- lua/cfg/test.lua
+++ lua/cfg/test.lua
@@ -3,27 +3,32 @@ local M = {}
 M.keys = {
   {
     "<leader>Tr",
-    require("neotest").run.run(),
+    function() 
+    require("neotest").run.run() end,
     desc = "Run nearest test",
   },
   {
     "<leader>Tf",
-    require("neotest").run.run(vim.fn.expand "%"),
+    function()
+    require("neotest").run.run(vim.fn.expand "%") end,
     desc = "Run tests in current file",
   },
   {
     "<leader>Td",
-    require("neotest").run.run { strategy = "dap" },
+    function()
+    require("neotest").run.run { strategy = "dap" } end,
     desc = "Debug nearest test",
   },
   {
     "<leader>Tq",
-    require("neotest").run.stop(),
+    function()
+    require("neotest").run.stop() end,
     desc = "Stop nearest test",
   },
   {
     "<leader>Ts",
-    require("neotest").summary.toggle(),
+    function()
+    require("neotest").summary.toggle() end,
     desc = "Toggle summary window",
   },
 }
